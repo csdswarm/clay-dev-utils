6c1c8fbf04a902cbefac32a92f25722ac2dbe863
Chris Strolia-Davis <48527154+csdswarm@users.noreply.github.com>
2021-04-22 14:49:54 -0400

    debugging

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ async function getUriComponentDataInPage() {
   const elements = document.querySelectorAll('[data-uri]');
   
   for (const el of elements) {
-    console.log(el.dataset.dataUri);
+    console.log(el);
+    console.log(el.dataset);
   }
 }
 
