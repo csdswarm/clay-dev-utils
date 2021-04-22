61c15011ce00fbb1d762296c869ef7166905bfd7
Chris Strolia-Davis <48527154+csdswarm@users.noreply.github.com>
2021-04-22 15:19:36 -0400

    enhancing functionality

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 async function getUriComponentDataInPage() {
   const elements = document.querySelectorAll('[data-uri]');
   
-  for (const el of elements) {
-    console.log(el.dataset.dataUri);
-  }
+  return await Promise.all([...elements].map(async el => {
+      let text;
+
+      try {
+          const res = await fetch('https://' + el.dataset.uri, { headers: { accept: 'application/json' }});
+          const data = await res.json();
+
+          return { el, data };
+
+      } catch (e) {
+          console.log('ERROR:', e);
+          return { el, data: {} }
+      }
+
+  }));
 }
 
 {
-  getUriComponentDataInPage();
+  getUriComponentDataInPage()
+    .then(results => console.log(results));
 }
